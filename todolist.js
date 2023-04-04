a13dc05867cf3e1901edeb25e317c310f450161b
ziziga10 <113436414+ziziga10@users.noreply.github.com>
2023-04-04 13:27:54 +0200

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,43 @@
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Kindly Enter Task Name!!!!")
+    }
+
+    else {
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <div>
+                    <button class="check">
+                        +
+                    </button>
+                    <button class="delete">
+                        X
+                    </button>
+                </div>
+            </div>
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for (let i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.parentNode.remove();
+            }
+        }
+        let checked_tasks = document.querySelectorAll(".task");
+        for (let i = 0; i < checked_tasks.length; i++) {
+            let checked = false;
+            let checkbutton = checked_tasks[i].querySelector('.check')
+            checkbutton.onclick = function () {
+                if (!checked) {
+                    checked_tasks[i].style.backgroundColor = '#c5e1c3';
+                } else {
+                    checked_tasks[i].style.backgroundColor = '#c5e1e6';
+                }
+                checked = !checked;
+            }
+        }
+    }
+}
